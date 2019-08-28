42a075e0f54bb7a74e340b7fc0eb8ab930708308
jlampar <jakub.lamparski@npspace.pl>
2019-08-28 13:58:54 +0200

    Updates

---
+++ precache-manifest.a9dbb4376ced2dd793a2657f33c9f23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c9b997baf8c82f6955807b8bbd7d80",
+    "url": "/landing-test/index.html"
+  },
+  {
+    "revision": "1c9276a783f3581c340d",
+    "url": "/landing-test/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "21eedd1ad7b2e8375a20",
+    "url": "/landing-test/static/css/main.15d9b66e.chunk.css"
+  },
+  {
+    "revision": "1c9276a783f3581c340d",
+    "url": "/landing-test/static/js/2.41505019.chunk.js"
+  },
+  {
+    "revision": "21eedd1ad7b2e8375a20",
+    "url": "/landing-test/static/js/main.00382b79.chunk.js"
+  },
+  {
+    "revision": "c708759489d85e4098f1",
+    "url": "/landing-test/static/js/runtime~main.0407917d.js"
+  }
+]);
